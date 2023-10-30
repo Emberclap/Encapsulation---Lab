f5f9f5056d99800d6df428757063d5b4ef25192e
Emberclap <133802297+Emberclap@users.noreply.github.com>
2023-10-30 15:04:08 +0200

    Add project files.

---
+++ PersonsInfo/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        
+        static void Main(string[] args)
+        {
+          
+          var n = int.Parse(Console.ReadLine());
+          Team team = new Team("SoftUni");
+          for (int i = 0; i < n; i++)
+          {
+              var cmdArgs = Console.ReadLine().Split();
+              var person = new Person(cmdArgs[0],
+              cmdArgs[1],
+              int.Parse(cmdArgs[2]),
+              decimal.Parse(cmdArgs[3], CultureInfo.InvariantCulture));
+              team.AddPlayer(person);
+          }
+          
+          
+          Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+          Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+
+            
+            
+        }
+    }
+}
